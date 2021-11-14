bfc01c4221bbd9881ebf75a5b34b4c7c8f236a6a
Brahim Chahbouni <brahimcah@gmail.com>
2021-11-14 15:03:17 +0100

    UPDATE

---
+++ UF1- Desenvolupament d'aplicacions per a dispositius mòbils/A12 - Habitants del Pla de l'Estany/app/src/main/java/com/brahimcah/habitantsdelpladelestany/MainActivity.java
@@ -0,0 +1,44 @@
+package com.brahimcah.habitantsdelpladelestany;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //Indiquem en aquet array el nom dels Habitants del Municipi
+    String[] municipis = new String[]{
+            "Banyoles", "Camós", "Cornellà del Terri",
+            "Crespià","Esponellà","Fontcoberta","Palol de Revardit","Porqueres","Sant Miquel de Campmajor"
+            ,"Serinyà","Vilademuls"
+    };
+    //Indiquem en aquet array el nº dels Habitants segons la psocio de cada municipi
+    String[] habitants = new String[]{
+            "17.451", "698", "2.106","247","441","1212","459","4.208","218","1.084","769"
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //AutoCompleteTextView
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, municipis);
+        AutoCompleteTextView atv_Municipi = (AutoCompleteTextView)
+                findViewById(R.id.atv_Municipi);
+        atv_Municipi.setAdapter(adapter);
+
+        final Button btn_1 = findViewById(R.id.btn_1);
+        final TextView atv_HABITANTS=findViewById(R.id.atv_HABITANTS);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Aquest és el codi que s'executa al clicar
+                atv_HABITANTS.setText("Has fet clic!");
+            }
+        });
+    }
+}
+
