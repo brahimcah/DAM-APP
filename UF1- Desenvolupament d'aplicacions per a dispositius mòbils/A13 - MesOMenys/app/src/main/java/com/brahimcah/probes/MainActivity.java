d1b368b50d445e84a3d662b89d091432f218aa99
Brahim Chahbouni <brahimcah@gmail.com>
2021-11-20 10:03:58 +0100

    update

---
+++ UF1- Desenvolupament d'aplicacions per a dispositius mòbils/A13 - MesOMenys/app/src/main/java/com/brahimcah/probes/MainActivity.java
@@ -0,0 +1,34 @@
+package com.brahimcah.probes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    String[] habitants = new String[]{
+            "-", "mico1.jpg", "mico2.jpg","mico3.jpg","mico4.jpg","1212","459","4.208","218","1.084","769"
+    };
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button btn_1 = findViewById(R.id.btn1);
+        final ImageView img1=findViewById(R.id.img1);
+        final ImageView img2=findViewById(R.id.img2);
+        img1.setImageResource(R.drawable.mico1);
+        img2.setImageResource(R.drawable.mico2);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
+
+
